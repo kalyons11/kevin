d192bbc2f4e0d9d34c10b559a1007ebefd0ae7bc
kalyons11 <kevinandrewlyons@gmail.com>
2019-12-16 23:21:12 -0500

    Fix bug with input number validation
    - Want at least 3 inputs
    - Was forcing at least 4
    - Change greater than to greater than or equal to

--- kevin/playground/read.py
+++ kevin/playground/read.py
@@ -4,7 +4,7 @@
 if __name__ == '__main__':
     # Read the number of inputs
     num_inputs = int(input("How many inputs? "))
-    assert num_inputs > 3, "At least 3 please."
+    assert num_inputs >= 3, "At least 3 please."
     print("Enter your {} inputs in the following form: inp1 inp2 ... inp{}".format(
         num_inputs, num_inputs))
     a = list(map(int, input().split()))
