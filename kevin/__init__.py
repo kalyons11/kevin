63442325922df78e98641a1830cc705c13a12163
kalyons11 <kevinandrewlyons@gmail.com>
2019-12-18 19:09:08 -0500

    Bump version to 0.1.0

--- kevin/__init__.py
+++ kevin/__init__.py
@@ -1,7 +1,5 @@
-"""
-Kevin's custom Python package for testing purposes.
-"""
+# Kevin's custom Python package for testing purposes.
 
-__version__ = '0.0.3'
+__version__ = '0.1.0'
 __author__ = 'Kevin Lyons'
 __description__ = 'Personal repository for development and experimentation.'
